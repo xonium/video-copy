322047d2fced93e6350da888130c62cd46981238
xonium <johan.erik.obrink@gmail.com>
2023-07-27 14:02:59 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React from "react";
+import { videoIds } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getEmbedCode = (videoId: string) => { 
+    const iframeSrc = `<iframe src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+   // Copy the text inside the text field
+   navigator.clipboard.writeText(iframeSrc);
+   // Alert the copied text
+   alert("kopierat koden\r\nklistra in med CTRL+V");
+  };
+
+  const renderVideo = (videoId: string) => {
+    return (
+      <>
+        <div style={{ flexGrow: 3, flexShrink: 1, display: "flex", flexDirection: "column", gap: "16px", backgroundColor: "gainsboro", padding: "16px" }}>
+          <iframe
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <div>
+            <button onClick={() => (getEmbedCode(videoId))}>Kopiera embed kod</button>
+          </div>
+        </div>
+      </>
+    );
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ display: "flex", gap: "16px", flexFlow: "row wrap" }}>
+        {videoIds.map((videoId: string) => renderVideo(videoId))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
